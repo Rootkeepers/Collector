7d8a0b405899f2e49058ea5cf97b516a266d7fc7
zkrxnp <cookcoo150@naver.com>
2026-06-29 00:43:31 +0900

    style: 주석 추가 및 함수화

--- Github-Collector.py
+++ Github-Collector.py
@@ -4,42 +4,32 @@ from github import Github
 TOKEN = os.getenv("GITHUB_TOKEN")
 g = Github(TOKEN)
 
-def check_github_rate_limit():
+def collect_rate_limit(): # GitHub API의 Rate Limit 상태를 확인 및 경고 메시지 출력
     overview = g.get_rate_limit()
     rate = overview.rate
-
-    reset_time = rate.reset.strftime("%Y-%m-%d %H:%M:%S")
+    reset_time = rate.reset.strftime("%Y-%m-%d %H:%M:%S") # 토큰 초기화 시간
 
     if rate.remaining == 0:
         print("\n[오류] GitHub API Rate Limit을 초과했습니다.")
         print(f"{reset_time} 시간 이후 다시 시도해주세요.")
+        exit()
     elif rate.remaining <= 10:
         print(f"\n[경고] 남은 API 요청 횟수가 {rate.remaining}회입니다.")
-    else:
-        print("\nAPI를 정상적으로 사용할 수 있습니다.")
 
-check_github_rate_limit()
-repo = g.get_repo("expressjs/express") # 패키지 리포 받아와야함
-# 테스트용 패키지
-tags = repo.get_tags() # 리포 태그 받아오기
-commit_info = []
+def collect_commit():
+    repo = g.get_repo("expressjs/express") # 패키지 리포 받아와야함
+    # 테스트용 패키지
+    tags = repo.get_tags() # 리포 태그 받아오기
+    commit_info = []
 
-for i, tag in enumerate(repo.get_tags()):
-    if i >= 10:
-        break
+    for i, tag in enumerate(repo.get_tags()):
+        if i >= 10:
+            break
 
-    commit = repo.get_commit(tag.commit.sha)
-
-    commit_info.append({
-        "tag": tag.name,
-        "sha": commit.sha,
-        "author": commit.commit.author.name,
-        "timestamp": commit.commit.author.date.strftime("%Y-%m-%d %H:%M:%S")
-    })
-
-for info in commit_info:
-    print(f"Tag: {info['tag']}")
-    print(f"SHA: {info['sha']}")
-    print(f"Author: {info['author']}")
-    print(f"Timestamp: {info['timestamp']}")
-    print()
+        commit = repo.get_commit(tag.commit.sha)
+        commit_info.append({
+            "tag": tag.name,
+            "sha": commit.sha,
+            "author": commit.commit.author.name,
+            "timestamp": commit.commit.author.date.strftime("%Y-%m-%d %H:%M:%S")
+        })
