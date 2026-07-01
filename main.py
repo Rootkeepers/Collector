4b7a40be3f3750e4d5fbd79e4b4ea20ff1f2bd7a
zkrxnp <cookcoo150@naver.com>
2026-07-01 17:58:07 +0900

    style: 파일 이름 변경

--- main.py
+++
@@ -1,7 +0,0 @@
-from Github_Collector import (get_package, collect_rate_limit, collect_commit,collect_workflow)
-
-get_package()
-collect_rate_limit()
-
-commit_info = collect_commit()
-workflow_info = collect_workflow()
